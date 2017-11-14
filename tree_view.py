7f65f13dad55235d730526be759d80bafe44d618
eathan <ssolsu@hotmail.com>
2017-11-14 20:00:50 +0800

    test

--- tree_view.py
+++ tree_view.py
@@ -7,6 +7,7 @@ window.geometry("%dx%d+%d+%d" % (width, height,
                                  (window.winfo_screenwidth() - width) / 2,
                                  (window.winfo_screenheight() - height) / 2))
 left_tree = ttk.Treeview(window,height=30)
+#添加作为标识
 primary_node = left_tree.insert("", 0, "出货成品", text="中国China", values=("1"))
 child_node = left_tree.insert(primary_node, 0, "广东", text="中国广东", values=("2"))
 left_tree.pack()
