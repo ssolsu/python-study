802fd9a9dfc2ca8b0d8ef1bb7bea05e5463bc3b1
Company_work <ssolsu1@126.com>
2017-11-14 17:35:02 +0800

    下午17:34

---
+++ tree_view.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+window = tk.Tk()
+window.title("tree_view")
+width, height = 800, 800
+window.geometry("%dx%d+%d+%d" % (width, height,
+                                 (window.winfo_screenwidth() - width) / 2,
+                                 (window.winfo_screenheight() - height) / 2))
+left_tree = ttk.Treeview(window,height=30)
+primary_node = left_tree.insert("", 0, "出货成品", text="中国China", values=("1"))
+child_node = left_tree.insert(primary_node, 0, "广东", text="中国广东", values=("2"))
+left_tree.pack()
+window.mainloop()
